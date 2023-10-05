1cc734784bb0a7bb0ea11be507045ec808728b96
Ntokozo Ndlovu <ntokozometo@gmail.com>
2023-10-05 10:50:03 +0200

    #9: create a user profile page

--- src/router/router.js
+++ src/router/router.js
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import SignUpPage from "../pages/SignUpPage";
 import HomePage from "../pages/HomePage";
+import UserProfile from "../pages/UserProfile";
 
 
 const rootRouter = createBrowserRouter([
     {path:'login',element: <LoginPage/>},
     {path:'signup', element:<SignUpPage/>},
-    {path:'home', element:<HomePage/>}
+    {path:'home', element:<HomePage/>},
+    {path:'userprofile', element: <UserProfile />}
   ])
 
   export default rootRouter;
