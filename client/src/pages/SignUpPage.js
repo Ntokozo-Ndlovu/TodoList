dec89b3720935f69b25707b4b67742bda3e7785b
Ntokozo Ndlovu <ntokozometo@gmail.com>
2023-10-20 14:26:26 +0200

    #31 add username on the app , #32 create a a sign up page

--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,5 +1,6 @@
 import classes from './SignUpPage.module.css';
 
+import { Form as FormRouter, redirect} from 'react-router-dom';
 import { Card, CardGroup, Form, FormLabel } from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
@@ -7,10 +8,35 @@ import { Col } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import CardHeader from 'react-bootstrap/esm/CardHeader';
 import { Link } from 'react-router-dom';
+import { useSubmit } from 'react-router-dom';
+import { useRef } from 'react';
+
 
 const SignUpPage = ()=>{
+    //reference for component
+    const nameRef = useRef();
+    const surnameRef = useRef();
+    const usernameRef = useRef();
+    const passwordRef = useRef();
+    const emailRef = useRef();
+    
+    const submit = useSubmit(); 
+ 
+ 
+ 
+    const handleSignUp = ()=>{
+        const user = {
+            name:nameRef.current.value,
+            surname:surnameRef.current.value,
+            email:emailRef.current.value,
+            username:usernameRef.current.value,
+            password:passwordRef.current.value
+        };
 
-    return <Form>
+        submit(user,{method:'POST', action:''})
+    }
+
+    return <FormRouter>
         <div style={{'backgroundColor':'#eee','height':'100vh' }} >
         <Container className='h-100'  fluid>
             <Row className='justify-content-center align-items-center h-100'>
@@ -22,11 +48,11 @@ const SignUpPage = ()=>{
                             </CardHeader>
                             <Card.Body>
                                 <FormLabel>Name:</FormLabel>
-                                <Form.Control type='text'/>
+                                <Form.Control ref={nameRef} type='text'/>
                                 <FormLabel>Surname:</FormLabel>
-                                <Form.Control type='text'/>
-                                <FormLabel>Email:</FormLabel>
-                                <Form.Control type='text'/>
+                                <Form.Control ref={surnameRef} type='text'/>
+                                <FormLabel> Email:</FormLabel>
+                                <Form.Control ref={emailRef} type='text'/>
                             </Card.Body>
                            </Card>
                     </div>
@@ -36,10 +62,10 @@ const SignUpPage = ()=>{
                                 User Auth
                             </CardHeader>
                             <Card.Body>
-                               <FormLabel>Username:</FormLabel>
+                               <FormLabel ref={usernameRef} >Username:</FormLabel>
                                 <Form.Control type='text'/>
                                 <FormLabel>Password:</FormLabel>
-                                <Form.Control type='text'/> 
+                                <Form.Control ref={passwordRef} type='text'/> 
                                 <FormLabel>Retype Password:</FormLabel>
                                 <Form.Control type='text'/>
                             </Card.Body>
@@ -53,15 +79,36 @@ const SignUpPage = ()=>{
                             </Link>
                              </Col>
                             <Col>
-                            <Button variant='primary' size="lg w-100">Sign Up</Button>
+                            <Button variant='primary' onClick={handleSignUp} size="lg w-100">Sign Up</Button>
                             </Col>
                           </Row>
                  </Col>
             </Row>
             </Container>
         </div>
-          </Form>
+    </FormRouter>
  
 }
 
 export default SignUpPage;
+
+export async function action({request}) {
+    const form = await request.formData();
+    const data = {
+        name:form.get('name'),
+        surname:form.get('surname'),
+        username:form.get('username'),
+        password:form.get('password'),
+        email:form.get('email')
+    }
+
+    const response = await fetch('http://localhost:3000/api/v1/auth/register',{method:'POST',body:JSON.stringify(data),headers:{'content-type':'application/json'}})
+    if(response.ok){
+        const {userId,token} = await response.json();
+        localStorage.setItem('token',token);
+        localStorage.setItem('userId',userId);
+        return redirect('/home')
+    }
+
+    return redirect(''); 
+}
